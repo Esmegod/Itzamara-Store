093c47017f7da1d611c755e58ab58cdbb6bb44b1
Esmeralda Godinez <egodinezm1600@alumno.ipn.mx>
2021-06-09 17:32:58 -0500

    Catálogos listo

--- assets/js/catalogos.js
+++ assets/js/catalogos.js
@@ -3,8 +3,8 @@ window.addEventListener("load", load, false);
 function load() {
     document.getElementById("op1").addEventListener("click", function () { mostrar(1); });
     document.getElementById("op2").addEventListener("click", function () { mostrar(2); });
-    document.getElementById("op3").addEventListener("click", function () { mostrar(3); });
     document.getElementById("btnCrear").addEventListener("click", function () { crear(); });
+    document.getElementById("btnRegresar").addEventListener("click", function () { regresar(); });
     //Se les asigana a todos los botones con Name="verProductos", la función mostrar(2);
     let botones = [];
     botones = document.getElementsByName("verProductos");
@@ -13,7 +13,6 @@ function load() {
     }
     document.getElementById("op1").style.color =  "#2d2b2b";
     document.getElementById("op2").style.color =  "#2d2b2b";
-    document.getElementById("op3").style.color =  "#2d2b2b";
     mostrar(1);
 }
 
@@ -22,40 +21,35 @@ function mostrar(i) {
         case 1:
             document.getElementById("misCatalogos").style.display = "block";
             document.getElementById("Crear").style.display = "none";
-            document.getElementById("agregarProductos").style.display = "none";
+            document.getElementById("verProductos").style.display = "none";
             document.getElementById("op1").style.color =  "#e7abad";
             document.getElementById("op2").style.color =  "#2d2b2b";
-            document.getElementById("op3").style.color =  "#2d2b2b";
             break;
         case 2:
             document.getElementById("misCatalogos").style.display = "none";
             document.getElementById("Crear").style.display = "block";
-            document.getElementById("agregarProductos").style.display = "none";
+            document.getElementById("verProductos").style.display = "none";
             document.getElementById("op1").style.color =  "#2d2b2b";
             document.getElementById("op2").style.color =  "#e7abad";
-            document.getElementById("op3").style.color =  "#2d2b2b";
             break;
-        case 3:
-                document.getElementById("misCatalogos").style.display = "none";
-                document.getElementById("Crear").style.display = "none";
-                document.getElementById("agregarProductos").style.display = "block";
-                document.getElementById("op1").style.color =  "#2d2b2b";
-                document.getElementById("op2").style.color =  "#2d2b2b";
-                document.getElementById("op3").style.color =  "#e7abad";
-                break;
     }
 }
 
 function verProductos() {
-    alert("Función en proceso");
+    document.getElementById("misCatalogos").style.display = "none";
+    document.getElementById("verProductos").style.display = "block";
 }
+
+function regresar(){
+    mostrar(1);
+}
+
 function crear() {
     form = document.getElementById("idCrear");
     if (!form.checkValidity()) {
-        alert("awas");
+        alert("Llenar todos los campos correctamente");
     }
     else{
-        alert("ah ok")
+        alert("Catálogo agregado");
     }
-
 }
